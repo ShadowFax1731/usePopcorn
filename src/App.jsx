149ddbacf7983858a52387fa1a82bfa14b937d4b
shadowfax <55938330+ShadowFax1731@users.noreply.github.com>
2024-11-14 04:27:15 +0530

    Passing elements as props, alternative to children

--- src/App.jsx
+++ src/App.jsx
@@ -119,7 +119,7 @@ function Movie({ movie }) {
   );
 }
 
-function Box({ children }) {
+function Box({ element }) {
   const [isOpen, setIsOpen] = useState(true);
 
   return (
@@ -127,7 +127,7 @@ function Box({ children }) {
       <button className="btn-toggle" onClick={() => setIsOpen((open) => !open)}>
         {isOpen ? "–" : "+"}
       </button>
-      {isOpen && children}
+      {isOpen && element}
     </div>
   );
 }
@@ -203,14 +203,24 @@ export default function App() {
         <NumberOfResults movies={movies} />
       </Navbar>
       <Main>
-        <Box>
+        <Box element={<List movies={movies} />}></Box>
+        <Box
+          element={
+            <>
+              <Summary watched={watched} />
+              <WatchList watched={watched} />
+            </>
+          }
+        ></Box>
+
+        {/* <Box>
           <List movies={movies} />
         </Box>
 
         <Box>
           <Summary watched={watched} />
           <WatchList watched={watched} />
-        </Box>
+        </Box> */}
       </Main>
     </>
   );
